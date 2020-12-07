768fafdd7d69c07ecb7cce898386d5a54d71cec3
lucky <takuto.yoshikai@gmail.com>
2020-12-07 11:21:31 +0900

    fix url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'parade',
     version = '1.0.0',
-    url = 'https://github.com/TakutoYoshikai/lina.git',
+    url = 'https://github.com/TakutoYoshikai/parade.git',
     license = 'MIT LICENSE',
     author = 'Takuto Yoshikai',
     author_email = 'takuto.yoshikai@gmail.com',
